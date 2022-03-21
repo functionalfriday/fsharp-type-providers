42255dfd03af474b2a8de28479da23e0b6d687f0
Patrick Drechsler <patrick@pdrechsler.de>
2022-03-21 22:22:37 +0100

    ✅ simple json works

--- TypeProviderDemos/TypeProviderDemos.Tests/Tests.fs
+++ TypeProviderDemos/TypeProviderDemos.Tests/Tests.fs
@@ -3,6 +3,8 @@ module Tests
 open Swensen.Unquote
 open Xunit
 open TypeProviderDemos
+open TypeProviderDemos.Demo1
+open TypeProviderDemos.Demo1
 
 [<Fact>]
 let ``My test`` () =
@@ -12,3 +14,18 @@ let ``My test`` () =
 let ``Can call other dll`` () =
     let result = Say.hello
     result =! "Hello World"
+
+[<Fact>]
+let ``simple json works`` () =
+
+    let bart =
+        """
+{
+firstname: "Bart",
+lastname: "Simpson",
+age: 10
+}
+"""
+    let actual = json2type bart
+    actual =! { Name = "Bart Simpson"; Age = 10 }
+    
--- TypeProviderDemos/TypeProviderDemos/Library.fs
+++ TypeProviderDemos/TypeProviderDemos/Library.fs
@@ -1,12 +1,23 @@
 ﻿namespace TypeProviderDemos
 
-open FSharp.Data
+open FSharp.Data.JsonProvider
 
 module Say =
     let hello =
         "Hello World"
         
 module Demo1 =
+    // target type
+    type Person = { Name: string; Age: int }
+    
+    // input type
+    type Simpsons = JsonProvider<"simpsons.json">
     
     let json2type json =
-        ""
+        let simpson = Simpsons.Parse json
+        let result =
+            {
+                Name = simpson.Firstname + " " + simpson.Lastname
+                Age = simpson.Age
+            }
+        result
